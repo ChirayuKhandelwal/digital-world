d7d68325eb8e635312bb09e206937c85de67e2fb
unknown <chirayu.2024ug1063@iiitranchi.ac.in>
2026-07-16 08:12:15 +0530

    Trim Firebase env vars to prevent trailing spaces

--- api/firebaseAdmin.js
+++ api/firebaseAdmin.js
@@ -15,8 +15,8 @@ if (!admin.apps.length) {
 
       admin.initializeApp({
         credential: admin.credential.cert({
-          projectId: process.env.FIREBASE_PROJECT_ID,
-          clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+          projectId: process.env.FIREBASE_PROJECT_ID.trim(),
+          clientEmail: process.env.FIREBASE_CLIENT_EMAIL.trim(),
           privateKey: privateKey,
         })
       });
